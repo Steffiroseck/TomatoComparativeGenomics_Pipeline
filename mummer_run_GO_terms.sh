88baeffee82475bc82fb693284edf1809633115c
Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>
2022-07-31 19:32:52 +0100

    Update mummer_run_GO_terms.sh

--- mummer_run_GO_terms.sh
+++ mummer_run_GO_terms.sh
@@ -4,20 +4,40 @@ These sequences are then run against the S. chilense salt/drought tolerant gene 
 
 # Path to current working directory
 wd=/home/steffi
+
 # Path to MUMmer software is stored in a variable
-mummer_path="/opt/bix/mummer/4.0.0/bin/nucmer"
+mummer_path="/opt/bix/mummer/4.0.0/bin"
 
-# Make a new directory to store the results from MUMmer analysis
+# Path to whole genome files
+lycopersicum=$wd/genomes/tomato/ITAG4.1_CDS.fasta
+lycopersicoides=$wd/genomes/Slycopersicoides/SlydLA2951_v2.0_cds.fasta
+pennellii=$wd/genomes/Spennellii/Spenn-v2-cds-annot.fa
+pimpinellifolium=$wd/genomes/Spimpinellifolium/Spimpinellifolium_genome.CDS.fa
+sitiens=$wd/genomes/Ssitiens/augustus.hints.mrna
+
+# Make a new directory to store the results from MUMmer analysis and save the directory to a variable
 mkdir $wd/Results/MUMmer_GOanalysis
+output_dir=$wd/Results/MUMmer_GOanalysis
 
-# Extract fasta sequences
-seqkit grep -f Results/chilense_lycopersicum/lycopersicumID /home/steffi/genomes/tomato/ITAG4.1_CDS.fasta > Results/nucmer_GOanalysis/lycopersicum.GOterms.matches.fasta
- 
-# Run nucmer alignment
-/opt/bix/mummer/4.0.0/bin/nucmer --mum --minmatch=50 --mincluster=100 --prefix=Results/nucmer_GOanalysis/chilense.lycopersicum.nucmer --threads=8 Results/chilense.GO.terms.salt.drought.IDs.fasta Results/nucmer_GOanalysis/lycopersicum.GOterms.matches.fasta
- 
+# Extract fasta sequences for the all the gene IDs from Blastn results.
+seqkit grep -f $wd/Results/chilense_lycopersicum/lycopersicumID $lycopersicum > $output_dir/lycopersicum.GOterms.matches.fasta
+seqkit grep -f $wd/Results/chilense_pennellii/pennelliiID $pennellii > $output_dir/pennellii.GOterms.matches.fasta
+seqkit grep -f $wd/Results/chilense_lycopersicoides/lycopersicoidesID $lycopersicoides > $output_dir/lycopersicoides.GOterms.matches.fasta
+seqkit grep -f $wd/Results/chilense_pimpinellifolium/pimpinellifoliumID $pimpinellifolium > $output_dir/pimpinellifolium.GOterms.matches.fasta
+seqkit grep -f $wd/Results/chilense_sitiens/sitiensID $sitiens > $output_dir/sitiens.GOterms.matches.fasta
+
+# Run nucmer alignment for each of the fasta file against S.chilense salt/drought GO terms fasta file
+cd $output_dir/
+for i in *.fasta; 
+do $mummer_path/nucmer  --mum --minmatch=50 --mincluster=100 --prefix=${i%.GOterms.matches.fasta*}.chilense --threads=8 $wd/Results/chilense.GO.terms.salt.drought.IDs.fasta ${i};
+done
+
 # Filter the alignments
-/opt/bix/mummer/4.0.0/bin/delta-filter -1 Results/nucmer_GOanalysis/chilense.lycopersicum.nucmer.delta > Results/nucmer_GOanalysis/chilense.lycopersicum.nucmer.delta.filter
+for i in *.delta;
+do $mummer_path/delta-filter -1 ${i} > ${i}.filter;
+done
  
-# Run show-diff
-/opt/bix/mummer/4.0.0/bin/show-diff -H -q Results/nucmer_GOanalysis/chilense.lycopersicum.nucmer.delta > Results/nucmer_GOanalysis/chilense.lycopersicum.nucmer.delta.diff #No inversions
+# Run show-diff to get the differences between the genome sequences
+for i in *.delta;
+$mummer_path/show-diff -H -q ${i} > ${i}.diff;
+done
