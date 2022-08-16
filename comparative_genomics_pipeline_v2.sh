535cc93b41fad3c4885a301e35f074a8e51d850d
Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>
2022-08-16 10:22:21 +0100

    Update comparative_genomics_pipeline_v2.sh
    Signed-off-by: Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>

--- comparative_genomics_pipeline_v2.sh
+++ comparative_genomics_pipeline_v2.sh
@@ -180,9 +180,18 @@
 # We also remove empty files which doesnt have any deleterious variants
   for i in $cl/*.proveanout $cp/*.proveanout $cly/*.proveanout $cpi/*.proveanout $cs/*.proveanout;
   do
-  awk '/# VARIATION/{p=1}p' ${i} | awk -F'\t' '$2 < -2.5' > ${i%.proveanout*}.deleterious | find  -type f -empty -delete
+  awk '/# VARIATION/{p=1}p' ${i} | awk -F'\t' '$2 < -2.5' > ${i%.proveanout*}.deleterious
   done
 
+# Provean analysis might generate empty files for the genes with no variants between the comparisons. You  can remove them by typing;
+  find  -type f -empty -delete
+  
+# Get the Top 5 genes with the highest number of deleterious variants
+  for i in $cl/*.deleterious $cp/*.deleterious $cly/*.deleterious $cpi/*.deleterious $cs/*.deleterious; 
+  do 
+  find . -name "*.deleterious" -type f | xargs wc -l | sort -rn | grep -v ' total$' | head -5 > Top5_deleterious_genes; 
+  done
+  
 # Generate an Upset plot to get the gene IDs intersections between different species in the study. 
 # A data table needs to be created with all the S.chilense IDs matched to the different species in the study. Once the data table is created, the R script to generate upset plot is saved as a separate script.
   paste $wd/Results/chilense_lycopersicum/chilenseID_lycopersicum $wd/Results/chilense_lycopersicoides/chilenseID_lycopersicoides $wd/Results/chilense_pennellii/chilenseID_pennellii $wd/Results/chilense_pimpinellifolium/chilenseID_pimpinellifolium $wd/Results/chilense_sitiens/chilenseID_sitiens > $wd/Results/GO.IDs.all.species
